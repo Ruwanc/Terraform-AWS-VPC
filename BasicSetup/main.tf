fb9712740c340cf020e71dffd92a3f68793c7755
Ruwanc <cg.ruwan@gmail.com>
2025-07-17 22:43:32 +0530

    V0.1.1 Fix the duplication error in vpc attachment to the internet gateway

--- BasicSetup/main.tf
+++ BasicSetup/main.tf
@@ -1,6 +1,9 @@
 #Create VPC
 resource "aws_vpc" "uat-vpc" {
   cidr_block = "10.1.0.0/16"
+  tags = {
+    Name = "UAT-VPC"
+  }
 }
 
 # Create Subnet1 - Public Subnet
@@ -22,15 +25,15 @@ resource "aws_internet_gateway" "uat-igw" {
   }
 }
 
-# Internet gatway attachment to VPC
-resource "aws_internet_gateway_attachment" "vpc-igw-attachment" {
-  internet_gateway_id = aws_internet_gateway.uat.id
-  vpc_id              = aws_vpc.uat-vpc.id
-}
+# Internet gatway attachment to VPC (This block is not reuired since the internet gateway is automatically attached to the VPC when creating the internet gateway with the vpc id tag)
+# resource "aws_internet_gateway_attachment" "vpc-igw-attachment" {
+#   internet_gateway_id = aws_internet_gateway.uat-igw.id
+#   vpc_id              = aws_vpc.uat-vpc.id
+# }
 
 # Create Route table for public subnet
 resource "aws_route_table" "uat-public-rt" {
-  vpc_id = aws_vpc.test.id
+  vpc_id = aws_vpc.uat-vpc.id
 
   # since this is exactly the route AWS will create, the route will be adopted
   route {
@@ -47,9 +50,9 @@ resource "aws_route_table_association" "route-table-association-with-public-subn
 
 # Create default route for public subnet
 resource "aws_route" "default_route_to_internet" {
-    route_table_id         = aws_route_table.uat-public-rt.id
-    destination_cidr_block = "0.0.0/0"
-    gateway_id             = aws_internet_gateway.uat-igw.id
+  route_table_id         = aws_route_table.uat-public-rt.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.uat-igw.id
 }
 
 # Create Network ACL for public subnet
@@ -58,25 +61,25 @@ resource "aws_network_acl" "nacl-public-subnet-1" {
 }
 
 resource "aws_network_acl_rule" "nacl-public-subnet-1-allow-all-ingress" {
-  network_acl_id = aws_network_acl.bar.id
+  network_acl_id = aws_network_acl.nacl-public-subnet-1.id
   rule_number    = 100
   egress         = false
   protocol       = "-1"
   rule_action    = "allow"
-  cidr_block     = "0.0.0/0"
+  cidr_block     = "0.0.0.0/0"
   #If the value of protocol is -1 or all, the from_port and to_port values will be ignored and the rule will apply to all ports
-#   from_port      = 22
-#   to_port        = 22
+  #   from_port      = 22
+  #   to_port        = 22
 }
 
 resource "aws_network_acl_rule" "nacl-public-subnet-1-allow-all-egress" {
-  network_acl_id = aws_network_acl.bar.id
+  network_acl_id = aws_network_acl.nacl-public-subnet-1.id
   rule_number    = 100
   egress         = true
   protocol       = "-1"
   rule_action    = "allow"
-  cidr_block     = "0.0.0/0"
+  cidr_block     = "0.0.0.0/0"
   #If the value of protocol is -1 or all, the from_port and to_port values will be ignored and the rule will apply to all ports
-#   from_port      = 22
-#   to_port        = 22
+  #   from_port      = 22
+  #   to_port        = 22
 }
